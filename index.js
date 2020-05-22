dba27a778141eb66df14936e4f7f6177823b365a
klarastrinic <45819882+klarastrinic@users.noreply.github.com>
2020-05-22 10:14:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-function weight(w,h){ return (w/h); }; var w=prompt("Enter your weight"); var h=prompt('enter your height'); var s=weight(w,h); console.log(s);
+function numberOfBottles(){
+
+    var bottle = 99;
+    
+    
+    
+    while(bottle > 0){
+    
+        var bottleWord = "bottle";
+    
+       
+    
+        console.log(bottle +" "+ bottleWord +" of beer on the wall");
+    
+        console.log(bottle +" "+ bottleWord +" on the wall");
+    
+        console.log("Take one down bottles on the wall");
+    
+        bottle -- ;
+    
+        if (bottle > 1){
+    
+            bottleWord = "bottles";
+    
+            }else
+    
+        if (bottle > 0){
+    
+            console.log(bottle +" "+ bottleWord +"bottles on the wall");
+    
+        }
+    
+        else{
+    
+            console.log("No more bottles on the wall");
+    
+        }
+    
+        }
+    
+    }
+    
+    numberOfBottles();
